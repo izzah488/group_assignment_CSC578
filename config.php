6f2e3cd47e406456445b9a56d290983a279a1236
wedanya <you@example.com>
2025-07-16 10:09:56 +0800

    danny3

--- config.php
+++ config.php
@@ -8,7 +8,7 @@ define('LOG_FILE_PATH', ROOT_PATH . 'logs/app_errors.log');
 define('DB_HOST', 'localhost');
 define('DB_NAME', 'money_mate_db');
 define('DB_USER', 'root');
-define('DB_PASS', 'danny3'); // CHANGE THIS!
+define('DB_PASS', ''); // CHANGE THIS!
 // You might also define other global constants here, e.g., site name
 // define('SITE_NAME', 'Money Mate');
 
